2b9a35396711c661433643af1ac86181722424fa
Joshua Serie <serie1jd@gmail.com>
2019-02-07 16:10:11 -0500

    1

--- Zeds.py
+++ Zeds.py
@@ -9,21 +9,43 @@ def normal():
           'The Zeds Virus is spreading rapidly!\n' +
           'Bob: You and I have to get to the CDC safe zone on the other side of the city.\n\n'
           )
-    choice_1 = input('Should we go down town[1] or through the suburbs[2]?')
-    if choice_1 == '1':
-        print(username + ": lets go down town!\n" +
-              "Bob: Alright, Let's!\n\n" +
-              'Bob: Should we go to the rooftops[1] or avoid parkour and take the Zed infested streets[2]\n\n')
-        choice_2 = input('')
-        if choice_2 == '1':
+    print(username + ": lets go down town!\n" +
+              "Bob: Alright, Let's!\n\n")
+    choice_2 = input('Bob: Should we go to the rooftops[1] or avoid parkour and take the Zed infested streets[2]?')
+    if choice_2 == '1':
             print(username + ': Lets parkour across the rooftops!' +
-                  'Narrator: After much parkouring,\n the duo reaches the end of the buildings\n with a low drop to the ground infront of them to a feild\n with the safe zone in sight.\n\n')
-                  choice_4 = input('Feeling confident, do they decide to jump down quickly[1] or climb down the building?[2]')
+                  'Narrator: After much parkouring,\n the duo reaches the end of the buildings\n with a low drop to the ground in front of them to a feild\n with the safe zone in sight.\n\n')
+            choice_4 = input('Feeling confident, do they decide to jump down quickly[1] or climb down the building?[2]')
             if choice_4 == '1':
                 print('Narrator: The two jumped down landing on a loud bag of cans, allerting the Zeds nearby\n' +
                       'Bob: I BROKE MY LEG HELP ME UP!!!\n' +
-                      username + ' noticed the Zeds approaching and left Bob and ran across the feild safely to the safe zone.'
+                      'Narrator: ' + username + ' noticed the Zeds approaching fast and left Bob as bait\n.' +
+                        'and ran across the field safely to the Safe Zone.\n\n' +
+                        "Bob's as bait was worth it!\n" + username + " survived!"
                       )
+                exit()
+            elif choice == '2':
+                print('Narrator: The duo climbed down a fire ladder quietly.\nThey ran safeley to the Safe Zone!\n\n' +
+                        userename + ' and Bob both survived')
+                exit()
+            elif choice_2 == '2':
+                print(username + ': Go down the roads! It is faster.\n\n')
+            choice_3 = input('Bob: should we take that running car[1] or try to out run them?[2]')
+            if choice_3 == '1':
+                print('Narrator: The two ignored a rule from ZombieLand: "Check the backseat."\n' +
+                          'There was a Zed back there.\nYou both died...')
+                exit()
+            elif choice_3 == '2':
+                print('Narrator: There was way to many Zeds down that road...\nYou died.')
+                exit()
+    else:
+        exit()
+
+
+
+
+
+
 
 
 
@@ -45,21 +67,16 @@ def short():
 
 def menu():
     print("Game Mode:\n" +
-          "1) Short\n" +
-          "2) Normal\n" +
-          "3) Exit\n")
+          "1) Normal\n" +
+          "2) Exit\n")
 
 
 def menu_make_choice(option):
     if option == "1":
         input("*Start Game*")
-        easy()
-
-    elif option == "2":
-        input("*Start Game*")
         normal()
 
-    elif option == "3":
+    elif option == "2":
         exit()
 
     else:
